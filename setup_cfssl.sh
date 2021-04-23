6606b255c7e548bb227ae8b6c4e6e0f449d1d4bc
Luca Steccanella <steccas@pm.me>
2021-04-23 17:53:57 +0200

    Integration with lemur + postgresql

--- setup_cfssl.sh
+++ setup_cfssl.sh
@@ -15,6 +15,8 @@ chown -R cfssl:cfssl /etc/cfssl
 go get -u github.com/cloudflare/cfssl/cmd/... #if doesn't work remove -u
 sudo cp -r /home/$USER/go/bin/* /usr/local/bin/
 
+
+
 #https://wiki.defect.ch/os/linux/lemur_and_cfssl_with_nginx_and_postgresql_on_centos8/lemur_and_cfssl_with_nginx_and_postgresql_on_centos8
 #https://github.com/Netflix/lemur-docker
 #https://github.com/mrts/docker-postgresql-multiple-databases
